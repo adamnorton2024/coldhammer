3be3f7bcb6002f58ddff1be6f0ba11abd629ea1e
Adam Norton <adam@adamnorton.com>
2020-01-18 00:52:32 -0600

    fixed responsive behaviors of artist cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,33 +14,45 @@ $('#flavorModal').on('show.bs.modal', function (event) {
     const flav_desc_ChocolatePeanutButter = "Sinfully decadent chocolate blended with savory peanut butter. What an amazing combination!";
     const flav_desc_GasLightCinnamon = "It is wonderfully fascinating and reminiscent of the warm sun on your face as a cool breeze carries the scent of a West Indies spice market.";
 
+    const img_flav_AlmondEnjoy = "./assets/images/label_almond.jpg";
+    const img_flav_BlackWidow = "./assets/images/label_blackwidow.jpg";
+    const img_flav_ChocolateCappuccino = "./assets/images/label_cappuccino.jpg";
+    const img_flav_ChocolatePeanutButter = "./assets/images/label_chocpeanut.jpg";
+    const img_flav_GasLightCinnamon = "./assets/images/label_cinnamon.jpg";
+
     let button = $(event.relatedTarget) // Button that triggered the modal
     let flavor = button.data('flavor') // Extract info from data-* attributes
     let flavorName = "";
     let proof = "70 Proof";
+    let label = "";
 
 
     switch (flavor) {
         case "cappuccino":
             flavorName = flav_name_ChocolateCappuccino;
             flavorDesc = flav_desc_ChocolateCappuccino;
+            label = img_flav_ChocolateCappuccino;
             break;
         case "chocpeanut":
             flavorName = flav_name_ChocolatePeanutButter;
             flavorDesc = flav_desc_ChocolatePeanutButter;
+            label = img_flav_ChocolatePeanutButter;
             break;
         case "almond":
             flavorName = flav_name_AlmondEnjoy;
             flavorDesc = flav_desc_AlmondEnjoy;
+            label = img_flav_AlmondEnjoy;
             break;
         case "blackwidow":
             flavorName = flav_name_BlackWidow;
             flavorDesc = flav_desc_BlackWidow;
+            label = img_flav_BlackWidow;
             proof = "107 Proof";
             break;
         case "cinnamon":
             flavorName = flav_name_GasLightCinnamon;
             flavorDesc = flav_desc_GasLightCinnamon;
+            label = img_flav_GasLightCinnamon;
             break;
         
         default:
@@ -52,4 +64,5 @@ $('#flavorModal').on('show.bs.modal', function (event) {
     modal.find('#flavor-name').text(flavorName);
     modal.find('#flavor-proof').text(proof);
     modal.find('#flavor-desc').text(flavorDesc);
+    modal.find('#flavor-label').attr('src', label);
 })
